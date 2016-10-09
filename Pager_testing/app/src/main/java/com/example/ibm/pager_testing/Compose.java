2412c8f6abaedce8da87468944dae6fbd751721f
adithyanarayanan <adithyan@buffalo.edu>
2016-10-09 11:38:40 -0400

    Added funct to Compose

--- Pager_testing/app/src/main/java/com/example/ibm/pager_testing/Compose.java
+++ Pager_testing/app/src/main/java/com/example/ibm/pager_testing/Compose.java
@@ -5,10 +5,37 @@ import android.os.Bundle;
 
 public class Compose extends AppCompatActivity
 {
+    EditText number;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+        number = (EditText)findViewById(R.id.numberType);
+        
     }
+    
+    public void numup(View v){
+    int pin = Integer.parseInt(number.getText() + "");
+    temp = pin + 1;
+    
+    number.setText(temp + "");
+    
+    }
+    
+    public void numdown(View v){
+    int pin = Integer.parseInt(number.getText() + "");
+    temp = pin - 1;
+    
+    number.setText(temp + "");
+    
+    }
+    
+//     public void back(View v){
+//     setContentView(R.layout.choosePIN)
+//     }
+
+//     public void back(View v){
+//     setContentView(R.layout.ReadMessages)
+//     }
 }
