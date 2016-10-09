663124bd7f3a1af18c8bcd71c6943a692e0295c4
adithyanarayanan <adithyan@buffalo.edu>
2016-10-09 11:21:21 -0400

    Added Functionality to Activity Second

--- Pager_testing/app/src/main/java/com/example/ibm/pager_testing/SignUp.java
+++ Pager_testing/app/src/main/java/com/example/ibm/pager_testing/SignUp.java
@@ -3,12 +3,41 @@ package com.example.ibm.pager_testing;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SignUp extends AppCompatActivity
-{
+public class SignUp extends AppCompatActivity {
+
+    TextView show;
+    EditText user;
+    int pin;
+    String name;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        user = (EditText)findViewById(R.id.username);
+        show = (TextView)findViewById(R.id.show);
+        name = String.valueOf(user.getText());
+
+
+
     }
+
+    public void generate(){
+        //generate a random 9 digit pin
+        Random rnd = new Random();
+        pin = rnd.nextInt(999999999) + 111111111;
+
+        show.setText("\t Your Username is: " + user + "\n \tyour PIN is:" + pin + "\n");
+    }
+
+    public void signup(){
+        //add to database
+
+    }
+
+
+
+
+
 }
