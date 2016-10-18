afac676de763355ff1e93839f96b35efe8a6a58f
jasonkimik <jasonkimik@users.noreply.github.com>
2016-10-18 18:36:20 -0400

    Update emailKey.php

--- emailKey.php
+++ emailKey.php
@@ -1,10 +1,11 @@
 <?php
-emailAddress =  $_GET['email'];
-$to      = emailAddress;
+$emailAddress =  $_GET['email'];
+$pin =  $_GET['tempPin'];
+$to      = $emailAddress;
 $key = uniqid(rand(5, 1000000), true);
 $subject = 'Key';
-$headers = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
+$headers = 'From: admin@teampageme.com' . "\r\n" .
+    'Reply-To: admin@teampageme.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, $key, $headers);
