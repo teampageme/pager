99dc0038907a92970e801a6caf81babea7f89c56
jasonkimik <jasonkimik@users.noreply.github.com>
2016-10-27 19:00:43 -0400

    Update getMsg.php

--- getMsg.php
+++ getMsg.php
@@ -3,13 +3,15 @@ $userID = htmlspecialchars($_GET["userNumber"]);
 $myFile = '/var/www/html/'. $userID . ".csv";
 if (file_exists($myFile)) 
 {
-     $fh = fopen($myFile, 'a');
-     while(! feof($file))
+     $fh = fopen($myFile, 'r');
+    while (($line = fgetcsv($fh)) !== FALSE) 
     {
-        print_r(fgetcsv($file));
+        print_r($line);
+        echo '</br>';
     }
     fclose($fh);
-    unlink($myfile);
+    #unlink($myfile);
+    exec('rm '.$myFile);
 }
 else
 {
