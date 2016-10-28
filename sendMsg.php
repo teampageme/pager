5ee551f8dbc6023506a27bb8c04fa91c377f84da
jasonkimik <jasonkimik@users.noreply.github.com>
2016-10-27 21:50:48 -0400

    Update sendMsg.php
    security issue solved

--- sendMsg.php
+++ sendMsg.php
@@ -1,18 +1,19 @@
 <?php
-#doesn't work
 $userID = htmlspecialchars($_GET["userNumber"]);
 $from = htmlspecialchars($_GET["from"]);
 $msg = htmlspecialchars($_GET["msgToSend"]);
 $time = date('c');
-$myFile = '/var/www/html/'. $userID . ".csv";
-echo $myFile;
-echo $time;
+$myFile = '/var/www/data/'. $userID . ".csv";
 $line = array($userID,$from,$time,$msg);
-if (file_exists($myFile)) {
+if (file_exists($myFile)) 
+{
   $fh = fopen($myFile, 'a');
-} else {
+} 
+else 
+{
   $fh = fopen($myFile, 'w');
 } 
 fputcsv($fh, $line);
+echo 'SENT'
 fclose($fh);
 ?>
